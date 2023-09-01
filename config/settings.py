06d757a52bd0392e3324e4526492d65ebc04df83
emediong <emediong5135@gmail.com>
2023-09-01 20:21:57 +0100

    login and logout views

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,10 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Account management
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
