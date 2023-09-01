16097cd86141932be4965c828450018a9c0383b4
emediong <emediong5135@gmail.com>
2023-09-01 20:36:39 +0100

    reset password views

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+
+# SMTP configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
